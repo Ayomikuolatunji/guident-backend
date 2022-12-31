938065f03781c4f0569ce0b92de3af08390cbe49
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-12-31 15:17:43 +0100

    fixing nodemailer production bug 4

--- dist/server.js
+++ dist/server.js
@@ -13,7 +13,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const cluster = require("cluster");
-const http = require("http");
 const numCPUs = require("os").cpus().length;
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
